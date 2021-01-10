18857171090f25ed8f4523c865cfa5a87f879486
adifakathy <adifakathy@gmail.com>
2021-01-10 20:38:10 +0600

    General Type of Python

---
+++ app.py
@@ -0,0 +1,49 @@
+print("hello world")
+print("My  name is Adifa Vuyia Kathi")
+print(1000)
+print("\"Adifa")
+#Casting
+x = str(3)
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+#Type()
+x = 5
+y = "kathi"
+z = 3.55
+print(type(x))
+print(type(y))
+print(type(z))
+#Unpack a collection
+fruits = ["apple","banana","mango"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+#output variable
+x = "My name is "
+y = "Adifa Vuyia Kathi"
+print(x + y)
+#Global variable
+x = "Awesome"
+def myfunc():
+    print("Python is " +x)
+
+myfunc()
+#Number type
+x = float(55643)
+y = int(5.000)
+z = complex(x)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+    
